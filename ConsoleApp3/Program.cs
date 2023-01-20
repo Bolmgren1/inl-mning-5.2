21e90c78ceacd2db4a8bca70363ff89b1de12f06
Acrobaticbag3 <sibe05001@utb.vaxjo.se>
2023-01-20 11:00:29 +0100

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många heltal vill du skriva in?");
+            int antal = int.Parse(Console.ReadLine());
+            string[] heltal = new string[antal];
+
+            for (int i = 0; i < antal; i++)
+            {
+                Console.WriteLine("Skriv in ditt heltal här");
+                heltal[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Talen du skrev in är:");
+
+            for (int i = 0; i < antal; i++)
+            {
+                Console.Write($"Tal {i + 1}: {heltal[i]} \n");
+            }
+
+        }
+    }
+}
